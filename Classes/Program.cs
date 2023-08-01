36701672720f2ae90d61083b94e16098dcce850c
Cali Weber <calista.weber1@gmail.com>
2023-07-31 20:27:25 -0700

    finished exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car = new Car();
+            car.Model = "Toyota";
+            car.Make = "Corolla";
+            car.Year = 2016;
+
+            Console.WriteLine($"This car is a {car.Model} {car.Make}, made in the year {car.Year}");
         }
     }
 }
